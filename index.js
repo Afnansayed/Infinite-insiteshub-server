183ceaa2d2922d71ab7d56ed5a5aa361af5ea28f
Afnansayed <afnansayed1973@gmail.com>
2024-05-14 18:40:52 +0600

    successfully send jwt token in client side

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = process.env.PORT|| 5000;
 
 //MIDDLE WIRE
-app.use(cors());
+app.use(cors({
+  origin:['http://localhost:5173'],
+  credentials: true,
+}));
 app.use(express.json());
-
+app.use(cookieParser());
 
 //MONGODB
 
@@ -24,6 +29,13 @@ const client = new MongoClient(uri, {
   }
 });
 
+//for cookie
+const cookieOption = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production" ? true: false,
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+ }
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -33,6 +45,22 @@ async function run() {
     const commentCollection = client.db('blogsDB').collection('allComments');
     const wishesCollection = client.db('blogsDB').collection('allWishes');
     const feedbackCollection = client.db('blogsDB').collection('allReview');
+
+   ///jwt
+    app.post('/jwt',async(req,res) => {
+      const user = req.body;
+      const token = jwt.sign(user , process.env.DB_TOKEN, {expiresIn: '1h'})
+      
+      res.cookie('token', token ,cookieOption)
+      res.send({success: true})
+})
+//log and delete cookie
+app.post('/logout', async(req,res) => {
+  const user = req.body;
+  console.log(user, 'delete token')
+  res.clearCookie('token', {...cookieOption, maxAge : 0 }).send({success:true})
+})
+   
     //Api related  data
    app.get('/allBlogs',async(req,res) =>{
         
@@ -60,7 +88,7 @@ async function run() {
 
     app.post('/allBlogs',async(req,res) => {
         const blog = req.body;
-        console.log(req.body);
+       // console.log(req.body);
         const result = await blogsCollection.insertOne(blog);
         res.send(result);
     })
@@ -114,9 +142,8 @@ async function run() {
     app.post('/allWishes', async(req,res)=>{
           const query = {title: req.query.title,userEmail: req.query?.userEmail}
       const existingWish = await wishesCollection.findOne(query);
-      console.log(existingWish)
+     //checking is exist or not
       if (existingWish) {
-          // If a similar document exists, return a message indicating duplicate data
           return res.status(400).send({message: "Already have this"})
       }
            const unFormat = new Date();
